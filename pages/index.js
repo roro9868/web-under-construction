927a64ec69ecef257b1b302e8d5dd592dfc121c8
Ro <99719677+roro9868@users.noreply.github.com>
2022-02-16 23:51:00 -0800

    Changed index page title and favicon

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,8 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Epist</title>
+        <link rel="icon" href="/favicon2.png" />
       </Head>
 
       <main>
